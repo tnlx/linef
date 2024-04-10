14706c0d9e0dae329f26a201cdf68ef1a2936300
Thuc Le <tcvle@proton.me>
2024-04-10 23:26:31 +0700

    Fix base url

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-    },
-    plugins: [
-      react(),
-      eslint({
-        cache: false,
-        include: ['./src/**/*.js', './src/**/*.jsx'],
-        exclude: [],
-      }),],
-  };
+export default defineConfig({
+  // eslint-disable-next-line no-undef
+  base: process.env.BASE,
+  build: {
+    outDir: 'build',
+  },
+  plugins: [
+    react(),
+    eslint({
+      cache: false,
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      exclude: [],
+    }),]
 });
